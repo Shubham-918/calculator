0f46cd71137fa16be6941b13bbb406e65feddfd1
Shubham-918 <104446672+Shubham-918@users.noreply.github.com>
2023-09-08 18:29:14 +0530

    3 files uploaded
    This repository consisting of 3 files index.html , style.css and script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const display = document.getElementById("display");
+
+document.getElementById("btn1").addEventListener("click",function(){
+    display.innerText = "1";
+});
+
+document.getElementById("btn2").addEventListener("click",function(){
+    display.innerText = "2";
+});
+
+document.getElementById("btn3").addEventListener("click",function(){
+    display.innerText = "3";
+});
+
+document.getElementById("btn4").addEventListener("click",function(){
+    display.innerText = "4";
+});
+
+document.getElementById("btn5").addEventListener("click",function(){
+    display.innerText = "5";
+});
+
+document.getElementById("btn6").addEventListener("click",function(){
+    display.innerText = "6";
+});
+
+document.getElementById("btn7").addEventListener("click",function(){
+    display.innerText = "7";
+});
+
+document.getElementById("btn8").addEventListener("click",function(){
+    display.innerText = "8";
+});
+
+document.getElementById("btn9").addEventListener("click",function(){
+    display.innerText = "9";
+});
